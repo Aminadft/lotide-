2d3ec9f14f76e695c940f6da5a91e1d6288247b0
Amina Malik <amina.malik@telus.com>
2022-11-17 12:36:10 -0500

    eqobjects

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+//function implementation
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚩Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const findKeyByValue = function(obj, val) {
+  for (let key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+//const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);//
