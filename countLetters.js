1ce2abb986b5bc357723d871ca08df69646ced34
Amina Malik <amina.malik@telus.com>
2022-11-17 12:22:50 -0500

    incrementing letters

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+//function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚩Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = function(allLetters) {
+  const results = {};
+  if (letter === " ") continue;
+  //i want to check results array
+  //add letters from results array
+  //get count of results array
+  if (results[letter]) {
+    results[letter] += 1;
+  } else {
+    //Initializing the letter with 1
+    results[letter] = 1;
+  }
+};
+return results;
