dd80e573d8aeca2c44fe13c8746576f85306fce5
Amina Malik <amina.malik@telus.com>
2022-11-21 22:32:44 -0500

    adding updating index

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  without,
+  takeUntil,
+  map,
+  letterPositions,
+  findKeyByValue,
+  findKey,
+  eqObjects,
+  eqArrays,
+  countLetters,
+  countOnly,
+  assertObjectsEqual,
+  assertEqual,
+  assertArraysEqual,
+
 };
